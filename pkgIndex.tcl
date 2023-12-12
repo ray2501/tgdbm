a32a70216d777952356c9350d2743a47b66b3494
Danilo Chang <ray2501@gmail.com>
2023-12-12 20:34:01 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,7 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded tgdbm 0.6 \
-    [list load [file join $dir libtgdbm0.6.so] tgdbm]
-package ifneeded qgdbm 0.6 \
-    [list source [file join $dir qgdbm.tcl]]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded tgdbm 0.6 \
+	    [list load [file join $dir libtcl9tgdbm0.6.so] [string totitle tgdbm]]
+} else {
+    package ifneeded tgdbm 0.6 \
+	    [list load [file join $dir libtgdbm0.6.so] [string totitle tgdbm]]
+}
