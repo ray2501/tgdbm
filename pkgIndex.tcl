ef42a0bf710f705e6e0f57fce61974d748386a0e
Danilo Chang <ray2501@gmail.com>
2023-12-12 21:15:48 +0800

    Update pkgIndex.tcl.in

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -8,3 +8,6 @@ if {[package vsatisfies [package provide Tcl] 9.0-]} {
     package ifneeded tgdbm 0.7 \
 	    [list load [file join $dir libtgdbm0.7.so] [string totitle tgdbm]]
 }
+
+package ifneeded qgdbm 0.7 \
+    [list source [file join $dir qgdbm.tcl]]
